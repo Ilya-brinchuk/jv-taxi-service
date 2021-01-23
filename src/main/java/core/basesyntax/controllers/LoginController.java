b7327a658d3ef58f158cdba5d8995b008644febe
ilya-brinchuk <ilya.brinchuk@gmail.com>
2021-01-23 20:46:23 +0200

    added correction

--- src/main/java/core/basesyntax/controllers/LoginController.java
+++ src/main/java/core/basesyntax/controllers/LoginController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class LoginController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("core.basesyntax");
     AuthenticationService<Driver> authenticationService = (AuthenticationService)
             injector.getInstance(AuthenticationService.class);
@@ -30,7 +31,7 @@ public class LoginController extends HttpServlet {
         try {
             Driver driver = authenticationService.login(login, pwd);
             HttpSession session = req.getSession();
-            session.setAttribute("driver_id",driver.getId());
+            session.setAttribute(DRIVER_ID,driver.getId());
         } catch (AuthenticationException e) {
             req.setAttribute("msg", e.getMessage());
             req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp);
